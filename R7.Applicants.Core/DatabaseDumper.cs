f72fc886a78a80f9aec53924e0234ae75294d0fa
roman-yagodin <roman.yagodin@gmail.com>
2020-07-07 23:28:08 +0400

    Extract class

---
+++ R7.Applicants.Core/DatabaseDumper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using LiteDB;
+using R7.Applicants.Core.Models;
+
+namespace R7.Applicants.Core
+{
+    public static class DatabaseDumper
+    {
+        public static void DumpDatabase (ILiteDatabase db)
+        {
+            Console.WriteLine ();
+            Console.WriteLine ("== Divisions:");
+            var divisions = db.GetCollection<Division> ("Divisions");
+            foreach (var division in divisions.FindAll ()) {
+                Console.WriteLine (division.Id + " " + division.Title);
+            }
+
+            Console.WriteLine ();
+            Console.WriteLine ("== EduForms:");
+            var eduForms = db.GetCollection<EduForm> ("EduForms");
+            foreach (var eduForm in eduForms.FindAll ()) {
+                Console.WriteLine (eduForm.Id + " " + eduForm.Title);
+            }
+
+            Console.WriteLine ();
+            Console.WriteLine ("== EduLevels:");
+            var eduLevels = db.GetCollection<EduLevel> ("EduLevels");
+            foreach (var eduLevel in eduLevels.FindAll ()) {
+                Console.WriteLine (eduLevel.Id + " " + eduLevel.Title);
+            }
+
+            Console.WriteLine ();
+            Console.WriteLine ("== Categories:");
+            var categories = db.GetCollection<Category> ("Categories");
+            foreach (var category in categories.FindAll ()) {
+                Console.WriteLine (category.Id + " " + category.Title);
+            }
+
+            Console.WriteLine ();
+            Console.WriteLine ("== EduPrograms:");
+            var eduPrograms = db.GetCollection<EduProgram> ("EduPrograms");
+            foreach (var eduProgram in eduPrograms.FindAll ()) {
+                Console.WriteLine ("Id: " + eduProgram.Id + ", EduLevelId: " + eduProgram.EduLevelId + ", Title: " + eduProgram.Title);
+            }
+
+            Console.WriteLine ();
+            Console.WriteLine ("== Applicants:");
+            var applicants = db.GetCollection<Applicant> ("Applicants");
+            foreach (var applicant in applicants.FindAll ()) {
+                Console.WriteLine ("Id: " + applicant.Id + ", Name: " + applicant.Name + ", EduProgramId: " + applicant.EduProgramId);
+            }
+        }
+    }
+}
